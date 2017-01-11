43fca1b45b68fa81a8421c1f7cf3ff6abb7d3f28
Emily Down <stripeytigers@gmail.com>
2017-01-11 17:25:30 +0000

    Added the capability to examine objects, expanded object definitions.

--- miaow.py
+++ miaow.py
@@ -1,12 +1,13 @@
 """You are a cat. You are hungry. A short adventure game."""
 
 # To-do list:
-# Add the capability to examine things.
+# Add a function which adds item to room as well as room to item
 # Add a human. The human should move between rooms in the house.
 # Add the capability to jump on things.
 # Add a mechanism to get food.
 # Add a mechanism to get in/out of patio doors.
 
+# Cat class with methods for interacting with the game
 class Cat(object):
     location = ''
 
@@ -30,50 +31,65 @@ class Cat(object):
             print "It would be rather chilly to sleep here. You'd prefer to sleep somewhere you can properly unwind."
         prompt()
 
+    def examine(self, item):
+        thing = self.loc_item(item)
+        if thing is not None:
+            print thing.description
+        else:
+            print "You can't see any " + item + " here." 
+        prompt()        
+
+    # This sub-function checks if the thing being interacted with is in the same
+    # room as the kitty. Returns the instance of the thing being interacted with
+    # if found, else returns None
+    def loc_item(self, item):
+        for thing in self.location.things:
+            if item in thing.synonyms:
+                return thing
+        return None
+                
+
 class Person(object):
     location = ''
 
 class Room(object):
-    def __init__(self, description, objects, inside):
-        self.description = description
-        self.objects = objects
-        self.inside = inside
     name = ''
-    description = ''
     exits = ''
     cat = ''
-    objects = ''
     people = ''
-    inside = True
+    def __init__(self, description, inside):
+        self.description = description
+        self.inside = inside
 
+class Thing(object):
+    def __init__(self, synonyms, description, jumpable):
+        self.synonyms = synonyms
+        self.description = description
+        self.jumpable = jumpable
+
+# Room definitions
 living = Room(
-    "Living Room\nYou are in a warm room with soft stuff under your paws and comfy places to curl up. You can see a raised cat bed on legs. There are shiny barriers to the south which are sometimes open and a doorway to the east.",
-    ['chair'],
+    "Living Room\nYou are in a warm room with soft stuff under your paws and comfy places to curl up. You can see a raised bed on legs. There are shiny barriers to the south which are sometimes open and a doorway to the east.",
     True)
 
 kitchen = Room(
     "Kitchen\nYou are in the room you associate with food. There is a raised platform where humans put their food when they eat. You are not supposed to jump on it. There is a tall thing in the corner that sometimes has stuff on. Your food bowl is in the corner. There is a doorway to the west and a flappy thing to the east.",
-    ['table', 'dresser', 'bowl'],
     True)
 
 path = Room(
     "Path\nA dark shadowy narrow place. You like it here. South is the bank, and the kitchen is to the west.",
-    ['bin'],
     False)
 
 bank = Room(
     "Bank\nAn overgrown area where you can creep under the shrubs. You sometimes find tasty mice here. To the west is the lawn, and to the north is the path.",
-    ['bush'],
     False)
 
 lawn = Room(
     "Lawn\nA wide open patch of grass with nowhere to hide. The patio is north and the bank is to the east.",
-    None,
     False)
 
 patio = Room(
     "Patio\nThis is an outside place, but with cold hard stuff under your paws. There's shiny stuff through which you can see the inside to the north, and sometimes get in. The lawn is to the south.",
-    ['flowerpot'],
     False)
 
 living.n = None
@@ -106,14 +122,69 @@ patio.e = None
 patio.s = lawn
 patio.w = None
 
+# Thing definitions
+chair = Thing(
+    ['chair', 'bed'],
+    "It smells warm and comfortable.",
+    True)
+
+table = Thing(
+    ['table'],
+    "You're not supposed to go up there. Apparently.",
+    True)
+
+dresser = Thing(
+    ['dresser', 'cupboard'],
+    "Sometimes your food comes out of there.",
+    True)
+
+bowl = Thing(
+    ['bowl', 'dish'],
+    "It is empty.",
+    False)
+
+wheelie = Thing(
+    ['bin', 'wheelie'],
+    "A useful high vantage point.",
+    True)
+
+bush = Thing(
+    ['bush', 'shrub'],
+    "Smells like teen spirit. Or mouse. One or the other.",
+    False)
+
+flowerpot = Thing(
+    ['flowerpot', 'pot'],
+    "It has some kind of green stuff in it.",
+    False)
+
+chair.location = living
+table.location = kitchen
+dresser.location = kitchen
+bowl.location = kitchen
+wheelie.location = path
+bush.location = bank
+flowerpot.location = patio
+
+living.things = [chair]
+kitchen.things = [table, dresser, bowl]
+path.things = [wheelie]
+bank.things = [bush]
+lawn.things = None
+patio.things = [flowerpot]
+
+# Input handling
 def prompt():
     input = raw_input(">")
-    if input in ['n', 'e', 's', 'w']:
+    input_args = input.split()
+    if input_args[0] in ['n', 'e', 's', 'w']:
         kitty.move(input)
-    if input in ['l', 'look']:
+    if input_args[0] in ['l', 'look']:
         kitty.look()
-    if input in ['sleep', 'z', 'zz', 'zzz', 'zzzz']:
+    if input_args[0] in ['sleep', 'z', 'zz', 'zzz', 'zzzz']:
         kitty.sleep()
+    if input_args[0] in ['x', 'examine']:
+        kitty.examine(input_args[1])
 
 bob = Person()
 bob.location = living
